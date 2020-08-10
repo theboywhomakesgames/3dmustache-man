cca6472e9eee4b1262376116a02a7479fba0f9f4
MH <mre.ezboy3@gmail.com>
2020-08-10 19:59:11 +0430

    animation rigging added

---
+++ Assets/Scripts/Utils/IndicatorPlacer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicatorPlacer : MonoBehaviour
+{
+    public float mouseSpeed = 10f;
+
+    private void LateUpdate()
+    {
+        transform.position += new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0) * Time.deltaTime * mouseSpeed;
+    }
+}
