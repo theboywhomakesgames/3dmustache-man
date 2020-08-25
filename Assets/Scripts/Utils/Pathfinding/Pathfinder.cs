f4c68b2b03330fe95df8a1add47484b6c1fedcbf
MH <mre.ezboy3@gmail.com>
2020-08-25 13:33:16 +0430

    pathfinder collision detection

--- Assets/Scripts/Utils/Pathfinding/Pathfinder.cs
+++ Assets/Scripts/Utils/Pathfinding/Pathfinder.cs
@@ -4,15 +4,103 @@ using UnityEngine;
 
 public class Pathfinder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector3 size, offset;
+    
+    public int layerMask;
+
+    [SerializeField]
+    private float _unitOffset, _unitRadius, _stepSize;
+
+    private bool[,] _units;
+    private bool _learned, _isRunning;
+
+    [SerializeField]
+    private float _time, _betweenUpdates;
+    private bool _isResting;
+
+    public void Learn()
     {
-        
+        _learned = true;
+        float x = _stepSize == 1 ? size.x : (size.x / _stepSize);
+        float y = _stepSize == 1 ? size.y : (size.y / _stepSize);
+
+        _units = new bool[(int)Mathf.Ceil(x), (int)Mathf.Ceil(y)];
+
+        print(_learned);
+        print(_units.GetLength(0) + " " + _units.GetLength(1));
     }
 
-    // Update is called once per frame
-    void Update()
+    private void PhysicalLearn()
     {
-        
+        int x = 0, y = 0;
+        for (float xOffset = -size.x / 2; xOffset < size.x / 2; xOffset += _stepSize)
+        {
+            y = 0;
+            for (float yOffset = -size.y / 2; yOffset < size.y / 2; yOffset += _stepSize)
+            {
+                _units[x, y] = Physics.CheckSphere(
+                    transform.position + offset + new Vector3(xOffset, yOffset),
+                    _unitRadius - _unitOffset,
+                    layerMask
+                );
+                y++;
+            }
+            x++;
+        }
+    }
+
+    private void OnDrawGizmos(){
+        Gizmos.DrawWireCube(transform.position + offset, size);
+
+        if(_stepSize > 0.05 && _isRunning)
+        {
+            int x = 0, y = 0;
+            for(float xOffset = -size.x/2; xOffset < size.x/2; xOffset += _stepSize){
+                y = 0;
+                for(float yOffset = -size.y/2; yOffset < size.y/2; yOffset += _stepSize){
+                    if(_units[x, y])
+                    {
+                        Gizmos.color = Color.black;
+                    }
+                    else
+                    {
+                        Gizmos.color = Color.white;
+                    }
+
+                    Gizmos.DrawWireSphere(
+                        transform.position + offset + new Vector3(xOffset, yOffset),
+                        _unitRadius - _unitOffset
+                    );
+                    y++;
+                }
+                x++;
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        if(!_learned)
+            Learn();
+
+        _isRunning = true;
+    }
+
+    private void Update()
+    {
+        if (_isResting)
+        {
+            _time += Time.deltaTime;
+            if(_time > _betweenUpdates)
+            {
+                _isResting = false;
+                _time = 0;
+            }
+        }
+        else
+        {
+            _isResting = true;
+            Invoke(nameof(PhysicalLearn), 0);
+        }
     }
 }
---
+++ Assets/Scripts/Utils/Pathfinding/PathfinderEditor.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Experimental.TerrainAPI;
+
+[CustomEditor(typeof(Pathfinder))]
+[CanEditMultipleObjects]
+public class PathfinderEditor : Editor
+{
+    static List<string> layers;
+    static string[] layerNames;
+
+    public static LayerMask LayerMaskField(string label, LayerMask selected)
+    {
+
+        if (layers == null)
+        {
+            layers = new List<string>();
+            layerNames = new string[4];
+        }
+        else
+        {
+            layers.Clear();
+        }
+
+        int emptyLayers = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            string layerName = LayerMask.LayerToName(i);
+
+            if (layerName != "")
+            {
+
+                for (; emptyLayers > 0; emptyLayers--) layers.Add("Layer " + (i - emptyLayers));
+                layers.Add(layerName);
+            }
+            else
+            {
+                emptyLayers++;
+            }
+        }
+
+        if (layerNames.Length != layers.Count)
+        {
+            layerNames = new string[layers.Count];
+        }
+        for (int i = 0; i < layerNames.Length; i++) layerNames[i] = layers[i];
+
+        selected.value = EditorGUILayout.MaskField(label, selected.value, layerNames);
+
+        return selected;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        Pathfinder myScript = (Pathfinder)target;
+        if(GUILayout.Button("Learn Level"))
+        {
+            myScript.Learn();
+        }
+
+        myScript.layerMask = LayerMaskField("Mask", myScript.layerMask);
+    }
+}
